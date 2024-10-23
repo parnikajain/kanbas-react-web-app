6ec4614776cf90eace2634633a488affe6b5b61f
parnikajain <20jainparnika@gmail.com>
2024-10-22 23:45:00 -0400

    part3

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,25 +1,54 @@
-import { Link, NavLink } from "react-router-dom";
+import { NavLink } from "react-router-dom";
+
 export default function CoursesNavigation() {
+  const links = [
+    { label: "Home", path: "/Kanbas/Courses/1234/Home" },
+    { label: "Modules", path: "/Kanbas/Courses/1234/Modules" },
+    { label: "Piazza", path: "/Kanbas/Courses/1234/Piazza" },
+    { label: "Zoom", path: "/Kanbas/Courses/1234/Zoom" },
+    { label: "Assignments", path: "/Kanbas/Courses/1234/Assignments" },
+    { label: "Quizzes", path: "/Kanbas/Courses/1234/Quizzes" },
+    { label: "People", path: "/Kanbas/Courses/1234/People" },
+  ];
+
   return (
     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
-      <NavLink 
-      to="/Kanbas/Courses/1234/Home" 
-      id="wd-course-home-link"
-      className="list-group-item border border-0"> 
-      Home 
-      </NavLink>
-      <NavLink to="/Kanbas/Courses/1234/Modules" id="wd-course-modules-link"
-        className="list-group-item text-danger border border-0"> Modules </NavLink>
-      <NavLink to="/Kanbas/Courses/1234/Piazza" id="wd-course-piazza-link"
-        className="list-group-item text-danger border border-0"> Piazza </NavLink>
-      <NavLink to="/Kanbas/Courses/1234/Zoom" id="wd-course-zoom-link"
-        className="list-group-item text-danger border border-0"> Zoom </NavLink>
-      <NavLink to="/Kanbas/Courses/1234/Assignments" id="wd-course-quizzes-link"
-        className="list-group-item text-danger border border-0"> Assignments </NavLink>
-      <NavLink to="/Kanbas/Courses/1234/Quizzes" id="wd-course-assignments-link"
-        className="list-group-item text-danger border border-0"> Quizzes </NavLink>
-      <NavLink to="/Kanbas/Courses/1234/People" id="wd-course-people-link"
-        className="list-group-item text-danger border border-0" > People </NavLink>
+      {links.map((link, index) => (
+        <NavLink 
+          key={index} 
+          to={link.path} 
+          id={`wd-course-${link.label.toLowerCase()}-link`}
+          className="list-group-item text-danger border border-0"
+        >
+          {link.label}
+        </NavLink>
+      ))}
     </div>
-);}
+  );
+}
+// import { Link, NavLink } from "react-router-dom";
+// export default function CoursesNavigation() {
+//   return (
+//     <div id="wd-courses-navigation" className="wd list-group fs-5 rounded-0">
+//       <NavLink 
+//       to="/Kanbas/Courses/1234/Home" 
+//       id="wd-course-home-link"
+//       className="list-group-item border border-0"> 
+//       Home 
+//       </NavLink>
+//       <NavLink to="/Kanbas/Courses/1234/Modules" id="wd-course-modules-link"
+//         className="list-group-item text-danger border border-0"> Modules </NavLink>
+//       <NavLink to="/Kanbas/Courses/1234/Piazza" id="wd-course-piazza-link"
+//         className="list-group-item text-danger border border-0"> Piazza </NavLink>
+//       <NavLink to="/Kanbas/Courses/1234/Zoom" id="wd-course-zoom-link"
+//         className="list-group-item text-danger border border-0"> Zoom </NavLink>
+//       <NavLink to="/Kanbas/Courses/1234/Assignments" id="wd-course-quizzes-link"
+//         className="list-group-item text-danger border border-0"> Assignments </NavLink>
+//       <NavLink to="/Kanbas/Courses/1234/Quizzes" id="wd-course-assignments-link"
+//         className="list-group-item text-danger border border-0"> Quizzes </NavLink>
+//       <NavLink to="/Kanbas/Courses/1234/People" id="wd-course-people-link"
+//         className="list-group-item text-danger border border-0" > People </NavLink>
+//     </div>
+// );}
+
 
--- src/Kanbas/Courses/index.tsx
+++ src/Kanbas/Courses/index.tsx
@@ -1,16 +1,20 @@
 import CoursesNavigation from "./Navigation";
+import { courses } from "../Database";
 import Modules from "./Modules";
 import Home from "./Home";
 import Assignments from "./Assignments";
 import AssignmentEditor from "./Assignments/Editor";
-import { Navigate, Route, Routes } from "react-router";
+import { Navigate, Route, Routes, useLocation, useParams } from "react-router";
 import { FaAlignJustify } from "react-icons/fa";
 export default function Courses() {
+  const { cid } = useParams();
+  const course = courses.find((course) => course._id === cid);
+  const { pathname } = useLocation();
     return (
       <div id="wd-courses">
   <h2 className="text-danger">
       <FaAlignJustify className="me-4 fs-4 mb-1" />
-      Course 1234 </h2> <hr />
+      {course && course.name} &gt; {pathname.split("/")[4]} </h2> <hr />
   <div className="d-flex">
     <div className="d-none d-md-block">
       <CoursesNavigation />
